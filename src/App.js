4343e1c8569fbc87c62a12dec1ad203e54ac572c
Ravikumar J <ravikumarj@Ravikumars-MacBook-Air.local>
2024-03-23 11:59:42 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
 function App() {
-  const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11]
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11];
   return (
     <div className="mainContainer">
-      {array.map((item) => (
-        <div key={item} className="miniContainer" />
-      ))}
+      {array.map((item) => {
+        return <div key={item} className="miniContainer" />;
+      })}
     </div>
   );
 }
