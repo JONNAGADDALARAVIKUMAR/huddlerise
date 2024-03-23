de95d9756f7ba757235cf80deb1c3aa89f4a268d
Ravikumar J <ravikumarj@Ravikumars-MacBook-Air.local>
2024-03-23 11:53:29 +0530

    New branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11]
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12]
   return (
     <div className="mainContainer">
       {array.map((item) => (
